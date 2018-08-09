a3fc47a721f5368a5faecada343a439f704a569e
jarretbryan <jarret.bryan@gmail.com>
2018-08-09 13:14:27 -0400

    render info based on isclicked state in pig

--- src/components/Pig.js
+++ src/components/Pig.js
@@ -3,27 +3,38 @@ import React, { Component } from 'react';
 class Pig extends Component {
 
     state = {
-        isClicked: true
+        isClicked: false
     }
     
-    reviewProps = () => {
-        debugger;
+    showStatsFn = () => {
+        let photoFileName;
+        photoFileName = this.props.pig.name.replace(/ /g, "_").toLowerCase()
+
+        if (this.state.isClicked === false){
+            return (<div className="ui eight wide column">
+                <h2>{this.props.pig.name}</h2>
+               
+                <img src={`../hog-imgs/${photoFileName}.jpg`} alt={this.props.pig.name} />
+
+            </div>)
+        } else {
+            return(
+                <div className="ui eight wide column">
+                    <h2>{this.props.pig.name}</h2>
+                    <p>Speciality: {this.props.pig.specialty}</p>
+                    <p>Highest Medal: {this.props.pig['highest medal achieved']}  </p>
+                    <p>Weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water: {this.props.pig['weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water']}  </p>
+                </div>
+            )
+        }
     }
     
     render() {
-        let photoFileName; 
-        photoFileName = this.props.pig.name.replace(/ /g, "_").toLowerCase()
+
         // debugger;
         return (
-            <div className="ui eight wide column">
-                <h2>{this.props.pig.name}</h2>
-                <p>Speciality: {this.props.pig.specialty}</p>
-                <img src= {`../hog-imgs/${photoFileName}.jpg`} alt={this.props.pig.name}/>
-                
-
-                {/* <img src = {`/public/hog-imgs/${photoFileName}.jpg' alt = ${this.props.pig.name}`}/> */}
-
-
+            <div>
+                {this.showStatsFn()}
             </div>
         );
     }
