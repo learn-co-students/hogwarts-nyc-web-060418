3b4cb74bf2d7c222549e1078804033a74278c72a
jarretbryan <jarret.bryan@gmail.com>
2018-08-09 12:50:30 -0400

    displaying pigs

--- src/components/Pig.js
+++ src/components/Pig.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 
 class Pig extends Component {
+
+    state = {
+        isClicked: true
+    }
     
     reviewProps = () => {
         debugger;
     }
     
     render() {
+        let photoFileName; 
+        photoFileName = this.props.pig.name.toLowerCase()
+        // debugger;
         return (
             <div className="ui eight wide column">
-                Hi
+                <h2>{this.props.pig.name}</h2>
+                <p>Speciality: {this.props.pig.specialty}</p>
+                <img src= {`../hog-imgs/${photoFileName}.jpg`} alt={this.props.pig.name}/>
+                
+
+                <img src = '../hog-imgs/porkchop.jpg' alt = "porkchop"/>
+
+
             </div>
         );
     }
